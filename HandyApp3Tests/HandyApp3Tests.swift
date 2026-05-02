80b5a30c958756f515bf9f6bdb62dc28163e6d09
hd <haoest@gmail.com>
2026-05-02 14:58:06 -0400

    Initial Commit

---
+++ HandyApp3Tests/HandyApp3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  HandyApp3Tests.swift
+//  HandyApp3Tests
+//
+//  Created by Hao Deng on 5/2/26.
+//
+
+import Testing
+@testable import HandyApp3
+
+struct HandyApp3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
